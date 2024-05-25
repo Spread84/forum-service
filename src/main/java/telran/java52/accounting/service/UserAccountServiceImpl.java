0f75385e9ea480d3b17b31b007b65ae909214fe1
semyonk <semyonk@localhost>
2024-05-25 21:33:38 +0300

    First ver after restart appl

---
+++ src/main/java/telran/java52/accounting/service/UserAccountServiceImpl.java
@@ -0,0 +1,105 @@
+package telran.java52.accounting.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java52.accounting.dao.UserAccountRepository;
+import telran.java52.accounting.dto.RolesDto;
+import telran.java52.accounting.dto.UserDto;
+import telran.java52.accounting.dto.UserEditDto;
+import telran.java52.accounting.dto.UserRegisterDto;
+import telran.java52.accounting.dto.exceptions.IncorrectRoleException;
+import telran.java52.accounting.dto.exceptions.UserExistsException;
+import telran.java52.accounting.dto.exceptions.UserNotFoundException;
+import telran.java52.accounting.model.Role;
+import telran.java52.accounting.model.UserAccount;
+
+@Service
+@RequiredArgsConstructor
+public class UserAccountServiceImpl implements UserAccountService, CommandLineRunner {
+
+	final UserAccountRepository userAccountRepository;
+	final ModelMapper modelMapper;
+
+	@Override
+	public UserDto register(UserRegisterDto userRegisterDto) {
+		if (userAccountRepository.existsById(userRegisterDto.getLogin())) {
+			throw new UserExistsException();
+		}
+		UserAccount userAccount = modelMapper.map(userRegisterDto, UserAccount.class);
+		String password = BCrypt.hashpw(userRegisterDto.getPassword(), BCrypt.gensalt());
+		userAccount.setPassword(password);
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto getUser(String login) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto removeUser(String login) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		userAccountRepository.delete(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public UserDto updateUser(String login, UserEditDto userEditDto) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		if (userEditDto.getFirstName() != null) {
+			userAccount.setFirstName(userEditDto.getFirstName());
+		}
+		if (userEditDto.getLastName() != null) {
+			userAccount.setLastName(userEditDto.getLastName());
+		}
+		userAccountRepository.save(userAccount);
+		return modelMapper.map(userAccount, UserDto.class);
+	}
+
+	@Override
+	public RolesDto changeRolesList(String login, String role, boolean isAddRole) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		boolean res;
+		try {
+			if (isAddRole) {
+				res = userAccount.addRole(role);
+			} else {
+				res = userAccount.removeRole(role);
+			}
+		} catch (Exception e) {
+			throw new IncorrectRoleException();
+		}
+		if (res) {
+			userAccountRepository.save(userAccount);
+		}
+		return modelMapper.map(userAccount, RolesDto.class);
+	}
+
+	@Override
+	public void changePassword(String login, String newPassword) {
+		UserAccount userAccount = userAccountRepository.findById(login).orElseThrow(UserNotFoundException::new);
+		String password = BCrypt.hashpw(newPassword, BCrypt.gensalt());
+		userAccount.setPassword(password);
+		userAccountRepository.save(userAccount);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if (!userAccountRepository.existsById("admin")) {
+			String password = BCrypt.hashpw("admin", BCrypt.gensalt());
+			UserAccount userAccount = new UserAccount("admin", "", "", password);
+			userAccount.addRole(Role.MODERATOR.name());
+			userAccount.addRole(Role.ADMINISTRATOR.name());
+			userAccountRepository.save(userAccount);
+		}
+
+	}
+
+}
---
+++ src/main/java/telran/java52/security/filter/AdminManagingRolesFilter.java
@@ -0,0 +1,40 @@
+package telran.java52.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import telran.java52.security.model.User;
+
+@Component
+@Order(20)
+public class AdminManagingRolesFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        if (checkEndpoint(request.getMethod(), request.getServletPath())) {
+            User user = (User) request.getUserPrincipal();
+            if (!user.getRoles().contains("ADMINISTRATOR")) {
+                response.sendError(403, "You are not allowed to access this resource");
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean checkEndpoint(String method, String path) {
+        return path.matches("/account/user/\\w+/role/\\w+");
+    }
+}
+
