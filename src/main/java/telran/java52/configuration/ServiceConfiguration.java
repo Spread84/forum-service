6b4c7ad412cabce70a97b00c8edd83a728708480
semyonk <semyonk@semyons-mbp>
2024-05-29 20:24:55 +0300

    J52 JT 11 Les. First after downloading from Edd git

--- src/main/java/telran/java52/configuration/ServiceConfiguration.java
+++ src/main/java/telran/java52/configuration/ServiceConfiguration.java
@@ -1,10 +1,14 @@
 package telran.java52.configuration;
 
+import java.net.PasswordAuthentication;
+
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ServiceConfiguration {
@@ -17,6 +21,11 @@ public class ServiceConfiguration {
 					.setFieldAccessLevel(AccessLevel.PRIVATE)
 					.setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
+	}
+	
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
